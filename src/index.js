9f70c917a853901db1c7f8d0353a7a11fc612e1d
Gabriele Lippi <gabriele@lippi.net>
2020-05-02 18:55:32 +0200

    Refactor index.js to use new functions and general improvements

--- src/index.js
+++ src/index.js
@@ -1,68 +1,34 @@
-import { removeComments, splitOnNewLine, removeIfNotACssClass, handleRulesWithCommas, transformToJsObject } from './utils'
+import { removeComments, splitOnNewLine, transformStylesheetToMap , pickStylesFrom } from './utils'
 
-window.addEventListener('load', function () {
+window.addEventListener('load', function() {
+  let stylesheet = "";
 
-let stylesheet = "";
-function loadTachyonsStylesheet() {    
-  fetch("https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css")
-    .then(r => r.text())
-    .then(tachyonsStyles => {
-      stylesheet = tachyonsStyles
-    })
-    .catch(e => window.alert("failed to load tachyons styles.\nPlease refresh the browser"));
-}
+  function loadTachyonsStylesheet() {    
+    fetch("https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css")
+      .then(r => r.text())
+      .then(tachyonsStyles => {
+        stylesheet =  transformStylesheetToMap(splitOnNewLine(removeComments(tachyonsStyles)));
+      })
+      .catch(e => console.error("failed to load tachyons styles.\nPlease refresh the browser", e));
+  }
 
-loadTachyonsStylesheet()
+  loadTachyonsStylesheet();
 
+  function copyToClipboard(classes) {
+    navigator.clipboard.writeText(classes);
+  }
 
-function transformToObject(stylesheet) {
-  const a = removeComments(stylesheet)
-  const b = splitOnNewLine(a)
-  const c = removeIfNotACssClass(b)
-  const d = handleRulesWithCommas(c)
-  const f = transformToJsObject(d)
-  console.log({ f })
-}
+  document.getElementById("translate").addEventListener("keyup", function() {
+    document.getElementById("new-rules").textContent = '';
+    const classesToTranslate = document.getElementById("translate").value;
+    const rules = pickStylesFrom(classesToTranslate, stylesheet);
 
-function checkValidityOf(expectedClassName, actualClassName) {
-  if (typeof expectedClassName === 'undefined') {
-    return `The class ${actualClassName} doesn't exist in Tachyons stylesheet`;
-  } else {
-    return expectedClassName;
-  };
-}
+    rules.split("\n").map(rule => {
+      return document.getElementById("new-rules").innerHTML += `<span class="db mv2 ph3">${rule}</span>`;
+    });
 
-function pickStylesFrom(classnames, stylesheet) {
-  if (classnames === "") return "";
-
-  const stylesMap = transformToObject(stylesheet);
-  const styles = classnames.split(" ").filter(d => d !== '');
-
-  if (styles.length > 1) {
-    return styles.reduce((acc, style, i) => {
-      return `${acc}${checkValidityOf(stylesMap[style], style)}${i === styles.length - 1 ? "" : "\n"}`
-    }, "");
-  };
-  return checkValidityOf(stylesMap[styles]);
-}
-
-
-function copyToClipboard(classes) {
-  navigator.clipboard.writeText(classes)
-}
-
-document.getElementById("translate").addEventListener("keyup", function() {
-  document.getElementById("new-rules").textContent = '';
-
-  const classesToTranslate = document.getElementById("translate").value;
-  const rules = pickStylesFrom(classesToTranslate, stylesheet);
-  rules.split("\n").map(rule => {
-    return document.getElementById("new-rules").innerHTML += `<span class="db mv2 ph3">${rule.replace(/"/g, "")}</span>`;
-  });
-
-  copyToClipboard(`.tachyonsIsAwesome {
+    copyToClipboard(`.tachyonsIsAwesome {
     ${rules}
-    }`)
-}); 
-
+    }`);
+  }); 
 })
